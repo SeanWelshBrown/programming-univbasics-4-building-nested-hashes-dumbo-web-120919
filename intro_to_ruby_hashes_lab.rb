c056fa49785312f777e13a2b654a4e7f33248a92
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-01 14:04:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,16 +4,15 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  {:railroads => 
-    {:pieces => 4}
+  {:railroads => {
+    :pieces => 4}
 	}
 end
 
 def monopoly_with_third_tier
-  {:railroads => 
-    {:pieces => 4, :rent_in_dollars => 
-      {:key1 => 1, }
-      
+  {:railroads => {
+    :pieces => 4, :rent_in_dollars => {
+      :key1 => 1, :key2 => 2, :key3 => 3, :key4 => 4}
     }
 	}
 end
